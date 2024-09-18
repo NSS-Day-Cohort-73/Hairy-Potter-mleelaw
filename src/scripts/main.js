912fd239a4342336ee6751695ff4355e6346f5fc
mleelaw <mleelaw123@gmail.com>
2024-09-18 15:45:48 -0500

    pottery pieces

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,14 @@
 // Imports go first
-
+import { makePottery } from "./PotteryWheel.js"
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let mug = makePottery("mug", 2, 4);
+let plate = makePottery ("plate", 6,1);
+let cup = makePottery("cup", 3, 5);
+let platter = makePottery("platter", 8,2);
+let vase = makePottery("vase", 9, 9);
+console.log(vase)
 
 
 // Fire each piece of pottery in the kiln
